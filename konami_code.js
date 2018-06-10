6076a636de50beef403f6e6977c41be37c12c156
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 21:56:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,13 +10,15 @@ const codes = [
   "b",
   "a"
 ];
-const matchingKeypresses = []
+let matchingKeypresses = []
 function init() {
   // your code here
 
   document.body.addEventListener('keydown', function(event) {
     if (codes[matchingKeypresses.length] === event.key){
       matchingKeypresses.push(event.key)
+    } else {
+      matchingKeypresses = []
     }
   })
 }
